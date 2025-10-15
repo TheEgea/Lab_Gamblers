d4f25dbb946e69b171f5cc574df635c5403434e5
Miquel Rodríguez Juvany <m.rodrijuv@gmail.com>
2025-10-15 11:28:16 +0200

    UserService

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/persistence/jpa/user/UserJpaRepository.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/persistence/jpa/user/UserJpaRepository.java
@@ -1,10 +1,15 @@
 package com.tecnocampus.LS2.protube_back.persistence.jpa.user;
 
+import com.tecnocampus.LS2.protube_back.domain.user.HashedPassword;
+import com.tecnocampus.LS2.protube_back.domain.user.RawPassword;
+import com.tecnocampus.LS2.protube_back.domain.user.User;
+import com.tecnocampus.LS2.protube_back.domain.user.Username;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findByUsername(String username);
+    Optional<User> loadByUsername(String email);
+    boolean verifyPassword(RawPassword raw, HashedPassword hashed);
 }
